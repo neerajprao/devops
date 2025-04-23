6440aa5e00e0805750966779f8ed0f80946ab111
Neeraj <153256352+neerajprao@users.noreply.github.com>
2025-04-23 10:08:10 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,5 @@
-# hello.py
+def main():
+    print("Hello from GitHub Actions!")
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, world from Docker + Kubernetes!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    main()
